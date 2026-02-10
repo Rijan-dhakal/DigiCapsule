cd747d0c69aa6572222c34dea48e218f1dee6ab8
Rijan Dhakal <dhakalrijan01@gmail.com>
2026-02-10 09:40:25 +0545

    feat: Implement google authentication

--- app/login/login-button.tsx
+++ app/login/login-button.tsx
@@ -2,15 +2,27 @@
 
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
+import { createAuthClient } from "better-auth/client";
 
 const LoginButton = () => {
+  const authClient = createAuthClient();
+  const signIn = async () => {
+    await authClient.signIn.social({
+      provider: "google",
+      callbackURL: "/",
+    });
+  };
+
   return (
-    <Button className="w-full bg-primary hover:bg-[#1f66f4a5] text-white py-6 text-base font-medium cursor-pointer">
-        <div className="flex items-center justify-center gap-4">
+    <Button
+      onClick={signIn}
+      className="w-full bg-primary hover:bg-[#1f66f4a5] text-white py-6 text-base font-medium cursor-pointer"
+    >
+      <div className="flex items-center justify-center gap-4">
         <FcGoogle className="w-6! h-6! size-6 font-bold" />
 
-      <span>Sign in with google</span>
-        </div>
+        <span>Sign in with google</span>
+      </div>
     </Button>
   );
 };
